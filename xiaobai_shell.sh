4fc1d027523b329329538b1240364533386e2d27
maxiaoba <maxiaoba@umich.edu>
2019-12-05 19:29:52 -0800

    test shell

--- xiaobai_shell.sh
+++ xiaobai_shell.sh
@@ -1,2 +1,3 @@
-tmux new-session -d -s 1 'source activate cs224w; python train_uci2.py --log_dir c0 --model_types EGSAGE_EGSAGE_EGSAGE --no
+source $CONDA_PREFIX/etc/profile.d/conda.sh
+tmux new-session -d -s 1 'conda activate cs224w; python train_uci2.py --log_dir c0 --model_types EGSAGE_EGSAGE_EGSAGE --no
 de_dim 15 --edge_dim 15 --epochos 200; python load_uci2.py --file c0'
