3375983d17a3a6678f4cc53f3243f8208e3ca538
maxiaoba <maxiaoba@umich.edu>
2019-12-05 19:24:17 -0800

    test shell

---
+++ xiaobai_shell.sh
@@ -0,0 +1,2 @@
+tmux new-session -d -s 1 'source activate cs224w; python train_uci2.py --log_dir c0 --model_types EGSAGE_EGSAGE_EGSAGE --no
+de_dim 15 --edge_dim 15 --epochos 200; python load_uci2.py --file c0'
