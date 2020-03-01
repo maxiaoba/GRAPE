95e7b18b70cd801cdc411a826222d2c349a5a1be
Daisy Yi Ding <dingd@stanford.edu>
2020-02-29 18:03:54 -0800

    construct matrices with missing values and run baselines for imputation

---
+++ run_baselines.py
@@ -0,0 +1,55 @@
+from fancyimpute import SimpleFill, KNN, IterativeImputer, IterativeSVD
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+from uci import get_dataset
+
+def construct_missing_X(missing_list, df):
+    nrow, ncol = df.shape
+    data_incomplete = np.zeros((nrow, ncol))
+    data_complete = np.zeros((nrow, ncol))
+    missing_ind = missing_list.reshape(nrow, ncol)
+    for i in range(nrow):
+        for j in range(ncol):
+            data_complete[i,j] = df.iloc[i,j]
+            if missing_ind[i,j]:
+                data_incomplete[i,j] = df.iloc[i,j]
+            else:
+                data_incomplete[i,j] = np.NaN
+    return data_complete, data_incomplete
+
+def impute_baselines(df, missing):
+    X, X_incomplete = construct_missing_X(missing, df)
+    n_missing = len(missing) - sum(missing)
+    
+    X_filled_mean = SimpleFill().fit_transform(X_incomplete)
+    MAE_mean = sum(sum(abs(X_filled_mean - X))) / n_missing
+    
+    X_filled_knn = KNN(k=3, verbose=False).fit_transform(X_incomplete)
+    diff_knn = X - X_filled_knn
+    MAE_knn = sum(sum(abs(diff_knn))) / n_missing
+    
+    X_filled_svd = IterativeSVD(rank=9,verbose=False).fit_transform(X_incomplete)
+    MAE_svd = sum(sum(abs(X_filled_svd - X))) / n_missing
+    
+    X_filled_mice = IterativeImputer().fit_transform(X_incomplete)
+    MAE_mice = sum(sum(abs(X_filled_mice - X))) / n_missing
+    
+    print("Mean absolute error (MAE) for recosntructing X using baseline imputation methods")
+    print("Mean: "+str(MAE_mean))
+    print("KNN: "+str(MAE_knn))
+    print("SVD: "+str(MAE_svd))
+    print("MICE: "+str(MAE_mice)) 
+
+    #return the imputed X matrices for downstream analysis
+    return (X_filled_mean, X_filled_knn, X_filled_svd, X_filled_mice)
+
+df_X = pd.read_csv('./Data/uci/housing/housing.csv')
+df_y = pd.read_csv('./Data/uci/housing/housing.csv', header=None)
+dataset = get_dataset(df_X, df_y, 0.7, 0.7, 0)
+missing_ind = dataset[0].known_edge_mask.numpy()
+
+#X, X_incomplete = construct_missing_X(missing, df)
+X_mean, X_knn, X_svd, X_mice = impute_baselines(df_X, missing_ind)
+print(X_mice)
