d0f3173ef734e5e8855a7ba25c3bd6f62dbd6660
Xiaobai Ma <xiaobaima@Xiaobais-MacBook-Pro.local>
2020-03-07 13:56:49 -0800

    add matrix_completion exps

---
+++ matric_completion/mc_data.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import os.path as osp
+from torch_geometric.data import Dataset
+from torch_geometric.data import Data
+from torch_geometric.data import DataLoader
+from torch_geometric.utils import undirected
+from sklearn import preprocessing
+
+import torch
+import random
+import numpy as np
+
+def create_node(df):
+    nrow, ncol = df.shape
+    feature_ind = np.array(range(ncol))
+    feature_node = np.zeros((ncol,ncol))
+    feature_node[np.arange(ncol), feature_ind] = 1
+    sample_node = [[1]*ncol for i in range(nrow)]
+    node = sample_node + feature_node.tolist()
+    return node
+
+def get_data(u_features, v_features, adj_train,
+    train_labels, train_u_indices, train_v_indices,
+    val_labels, val_u_indices, val_v_indices, 
+    test_labels, test_u_indices, test_v_indices, 
+    class_values):
+
+    n_row, n_col = adj_train.shape
+    if (u_features is None) or (v_features is None):
+        x = torch.tensor(create_node(adj_train), dtype=torch.float)
+    else:
+        print("using given feature")
+        x = torch.cat((torch.tensor(u_features,dtype=torch.float),
+                        torch.tensor(v_features,dtype=torch.float)),
+                        dim=0)
+
+    train_v_indices = train_v_indices + n_row
+    train_edge_index = torch.tensor([np.append(train_u_indices,train_v_indices),
+                        np.append(train_v_indices,train_u_indices)],
+                        dtype=int)
+    train_edge_attr = torch.tensor(np.append(train_labels,train_labels)[:,None],
+                        dtype=torch.float)
+
+    val_v_indices = val_v_indices + n_row
+    val_edge_index = torch.tensor([np.append(val_u_indices,val_v_indices),
+                        np.append(val_v_indices,val_u_indices)],
+                        dtype=int)
+    val_edge_attr = torch.tensor(np.append(val_labels,val_labels)[:,None],
+                        dtype=torch.float)
+
+    test_v_indices = test_v_indices + n_row
+    test_edge_index = torch.tensor([np.append(test_u_indices,test_v_indices),
+                        np.append(test_v_indices,test_u_indices)],
+                        dtype=int)
+    test_edge_attr = torch.tensor(np.append(test_labels,test_labels)[:,None],
+                        dtype=torch.float)
+
+    data = Data(x=x,
+            train_edge_index=train_edge_index,train_edge_attr=train_edge_attr,
+            val_edge_index=val_edge_index,val_edge_attr=val_edge_attr,
+            test_edge_index=test_edge_index,test_edge_attr=test_edge_attr,
+            )
+    return data
+
+def split_edge(edge_attr,edge_index,batch_size):
+    edge_num = int(edge_attr.shape[0]/2)
+    perm = np.random.permutation(edge_num)
+    edge_attrs, edge_indexes = [],[]
+    index = 0
+    while index + batch_size < edge_num:
+        edge_attr_i = torch.cat((edge_attr[index:index+batch_size,:],
+                                edge_attr[index:index+batch_size,:]),dim=0)
+        edge_start_i = torch.cat([edge_index[0,index:index+batch_size],
+                                    edge_index[1,index:index+batch_size]])
+        edge_end_i = torch.cat([edge_index[1,index:index+batch_size],
+                                    edge_index[0,index:index+batch_size]])
+        edge_index_i = torch.tensor([edge_start_i.detach().numpy(),
+                                    edge_end_i.detach().numpy()],
+                                    dtype=int)
+        index += batch_size
+        edge_attrs.append(edge_attr_i)
+        edge_indexes.append(edge_index_i)
+    return edge_attrs, edge_indexes
+
+
--- matric_completion/train_mc.py
+++ matric_completion/train_mc.py
@@ -1,6 +1,14 @@
 import time
 import argparse
 import os
+import sys, copy, math, time, pdb, warnings, traceback
+import pickle
+import scipy.io as sio
+import scipy.sparse as ssp
+import os.path
+import random
+from shutil import copy, rmtree, copytree
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 import networkx as nx
 import numpy as np
@@ -17,12 +25,16 @@ from torch_geometric.data import DataLoader
 
 import torch_geometric.nn as pyg_nn
 
+import sys
+sys.path.append("..")
 from utils import build_optimizer, objectview, get_known_mask, mask_edge
-from uci import get_data
 
 from gnn_model import GNNStack
 from prediction_model import MLPNet
-from plot_utils import plot_result
+from mc_plot_utils import plot_result
+
+from preprocessing import *
+from mc_data import get_data
 
 def train(data, args, log_path):
     # build model
@@ -39,10 +51,7 @@ def train(data, args, log_path):
     # train
     Train_loss = []
     Valid_mse = []
-    Valid_l1 = []
-
-    best_test_l1 = np.inf
-    best_epoch = None
+    Valid_rmse = []
 
     x = data.x.clone().detach()
     train_edge_index = data.train_edge_index.clone().detach()
@@ -66,7 +75,7 @@ def train(data, args, log_path):
 
         opt.zero_grad()
         x_embd = model(x, known_edge_attr, known_edge_index)
-        pred = impute_model([x_embd[train_edge_index[0]],x_embd[train_edge_index[1]]])
+        pred = impute_model([x_embd[train_edge_index[0],:],x_embd[train_edge_index[1],:]])
         pred_train = pred[:int(train_edge_attr.shape[0]/2)]
         label_train = train_edge_attr[:int(train_edge_attr.shape[0]/2)]
 
@@ -84,16 +93,15 @@ def train(data, args, log_path):
         label_test = test_edge_attr[:int(test_edge_attr.shape[0]/2)]
         mse = F.mse_loss(pred_test, label_test, 'mse')
         test_mse = mse.item()
-        l1 = F.l1_loss(pred_test, label_test, 'l1')
-        test_l1 = l1.item()
+        test_rmse = np.sqrt(test_mse)
 
         Train_loss.append(train_loss)
         Valid_mse.append(test_mse)
-        Valid_l1.append(test_l1)
+        Valid_rmse.append(test_rmse)
         print('epoch: ',epoch)
         print('loss: ',train_loss)
         print('test mse: ',test_mse)
-        print('test l1: ',test_l1)
+        print('test rmse: ',test_rmse)
 
     pred_train = pred_train.detach().numpy()
     label_train = label_train.detach().numpy()
@@ -104,7 +112,7 @@ def train(data, args, log_path):
     obj['args'] = args
     obj['train_loss'] = Train_loss
     obj['test_mse'] = Valid_mse
-    obj['test_l1'] = Valid_l1
+    obj['test_rmse'] = Valid_rmse
     obj['pred_train'] = pred_train
     obj['label_train'] = label_train
     obj['pred_test'] = pred_test
@@ -118,14 +126,26 @@ def train(data, args, log_path):
     plot_result(obj, log_path)
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--uci_data', type=str, default='housing') # 'pks', 'cancer', 'housing', 'wine'
+    # Arguments
+    parser = argparse.ArgumentParser(description='Inductive Graph-based Matrix Completion')
+    # general settings
+    parser.add_argument('--testing', action='store_true', default=True,
+                        help='if set, use testing mode which splits all ratings into train/test;\
+                        otherwise, use validation model which splits all ratings into \
+                        train/val/test and evaluate on val only')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='turn on debugging mode which uses a small number of data')
+    parser.add_argument('--data-name', default='douban', help='dataset name')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--use-features', action='store_true', default=False,
+                        help='whether to use node features (side information)')
     parser.add_argument('--model_types', type=str, default='EGSAGE_EGSAGE_EGSAGE')
     parser.add_argument('--node_dim', type=int, default=64)
     parser.add_argument('--edge_dim', type=int, default=64)
     parser.add_argument('--edge_mode', type=int, default=1) # 0: use it as weight 1: as input to mlp
     parser.add_argument('--impute_hiddens', type=str, default='64')
-    parser.add_argument('--epochs', type=int, default=20000)
+    parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--opt', type=str, default='adam')
     parser.add_argument('--opt_scheduler', type=str, default='none')
     parser.add_argument('--opt_restart', type=int, default=0)
@@ -134,31 +154,81 @@ def main():
     parser.add_argument('--dropout', type=float, default=0.)
     parser.add_argument('--weight_decay', type=float, default=0.)
     parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--train_edge', type=float, default=0.7)
-    parser.add_argument('--train_y', type=float, default=0.7)
     parser.add_argument('--known', type=float, default=0.7)
-    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--log_dir', type=str, default='0')
+    '''
+        Set seeds, prepare for transfer learning (if --transfer)
+    '''
     args = parser.parse_args()
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+    print(args)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+
+    rating_map, post_rating_map = None, None
+
+    u_features, v_features, adj_train, train_labels, train_u_indices, train_v_indices, \
+        val_labels, val_u_indices, val_v_indices, test_labels, \
+        test_u_indices, test_v_indices, class_values = load_data_monti(args.data_name, args.seed, args.testing, rating_map, post_rating_map)
+
+    print('All ratings are:')
+    print(class_values)
+    '''
+    Explanations of the above preprocessing:
+        class_values are all the original continuous ratings, e.g. 0.5, 2...
+        They are transformed to rating labels 0, 1, 2... acsendingly.
+        Thus, to get the original rating from a rating label, apply: class_values[label]
+        Note that train_labels etc. are all rating labels.
+        But the numbers in adj_train are rating labels + 1, why? Because to accomodate neutral ratings 0! Thus, to get any edge label from adj_train, remember to substract 1.
+        If testing=True, adj_train will include both train and val ratings, and all train data will be the combination of train and val.
+    '''
+
+    if args.use_features:
+        u_features, v_features = u_features.toarray(), v_features.toarray()
+        n_features = u_features.shape[1] + v_features.shape[1]
+        print('Number of user features {}, item features {}, total features {}'.format(u_features.shape[1], v_features.shape[1], n_features))
+    else:
+        u_features, v_features = None, None
+        n_features = 0
+
+    if args.debug:  # use a small number of data to debug
+        num_data = 1000
+        train_u_indices, train_v_indices = train_u_indices[:num_data], train_v_indices[:num_data]
+        train_labels = train_labels[:num_data]
+        val_u_indices, val_v_indices = val_u_indices[:num_data], val_v_indices[:num_data]
+        val_labels = val_labels[:num_data]
+        test_u_indices, test_v_indices = test_u_indices[:num_data], test_v_indices[:num_data]
+        test_labels = test_labels[:num_data]
+
+    print('#train: %d, #val: %d, #test: %d' % (len(train_u_indices), len(val_u_indices), len(test_u_indices)))
+
+    '''
+        Transfer to torch geometric Data
+    '''
+    data = get_data(u_features, v_features, adj_train,
+        train_labels, train_u_indices, train_v_indices, \
+        val_labels, val_u_indices, val_v_indices, test_labels, \
+        test_u_indices, test_v_indices, class_values)
+
+    '''
+        Train and apply the GNN model
+    '''
     args.model_types = args.model_types.split('_')
     if args.impute_hiddens == '':
         args.impute_hiddens = []
     else:
         args.impute_hiddens = list(map(int,args.impute_hiddens.split('_')))
 
-    seed = args.seed
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    
-    df_X = pd.read_csv('./Data/uci/'+ args.uci_data +"/"+ args.uci_data +'.csv')
-    df_y = pd.read_csv('./Data/uci/'+ args.uci_data +"/"+ args.uci_data +'_target.csv', header=None)
-    data = get_data(df_X, df_y, args.train_edge, args.train_y, args.seed)
-
-    log_path = './Data/uci/'+args.uci_data+'/'+args.log_dir+'/'
+    log_path = './Data/'+args.data_name+'/'+args.log_dir+'/'
     os.mkdir(log_path)
 
     train(data, args, log_path) 
 
 if __name__ == '__main__':
     main()
+
+
+
 
