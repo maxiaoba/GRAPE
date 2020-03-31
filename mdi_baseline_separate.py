e3862f589f60783e30ffe2737a1f83e37dacb3b0
Daisy Yi Ding <dingd@stanford.edu>
2020-03-31 21:47:07 +0800

    add training for separate data

---
+++ mdi_baseline_separate.py
@@ -0,0 +1,77 @@
+import time
+import argparse
+import os
+
+import networkx as nx
+import numpy as np
+import torch
+import torch.optim as optim
+import pandas as pd
+import torch.nn.functional as F
+import joblib
+import pickle
+
+from torch_geometric.datasets import TUDataset
+from torch_geometric.datasets import Planetoid
+from torch_geometric.data import DataLoader
+
+import torch_geometric.nn as pyg_nn
+
+from utils import build_optimizer, objectview, get_known_mask, mask_edge, auto_select_gpu
+from uci import get_data
+
+from gnn_model import GNNStack
+from prediction_model import MLPNet
+from plot_utils import plot_result
+
+from baseline import baseline_inpute
+from sklearn.linear_model import LinearRegression
+from utils import construct_missing_X, get_impute_mae
+
+def train(data, method):
+    train_edge_mask = data.train_edge_mask.numpy()
+    X, X_incomplete = construct_missing_X(train_edge_mask, data.df_X)
+
+    X_filled = baseline_inpute(data, method)
+    mae = get_impute_mae(X,X_filled,train_edge_mask.reshape(X.shape))
+
+    return mae
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--uci_data', type=str, default='housing') # 'pks', 'cancer', 'housing', 'wine'
+    parser.add_argument('--train_edge', type=float, default=0.7)
+    parser.add_argument('--train_y', type=float, default=0.7)
+    parser.add_argument('--comment', type=str, default='v1')
+    parser.add_argument('--seed', type=int, default=0)
+    args = parser.parse_args()
+    # device is cpu by default
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    df_np = pd.read_csv('./Data/RNAseq/rnaseq_normalized.csv', header=None, sep=" ")
+    df_X = pd.DataFrame(df_np).iloc[:,0:300] #extract a subset for experiment
+    df_y = pd.DataFrame([0] * df_X.shape[0])
+
+    train_fraction = 0.7
+    train_index = np.random.uniform(0,1,df_X.shape[0]) < train_fraction
+    df_X_train = df_X[train_index]
+    df_X_test = df_X[~train_index]
+    df_y_train = df_y[train_index]
+    df_y_test = df_y[~train_index]
+
+    data = get_data(df_X_test, df_y_train, args.train_edge, args.train_y, args.seed, normalize=False)
+    for method in ['mean', 'knn', 'svd', 'mice']:
+        log_path = './Data/mdi_results/{}_{}/rnaseq/{}/'.format(method, args.comment, args.seed)
+        if not os.path.isdir(log_path):
+            os.makedirs(log_path)
+        mae = train(data, method)
+
+        with open("{}results.txt".format(log_path), "w") as text_file:
+            text_file.write('{}'.format(mae))
+
+
+if __name__ == '__main__':
+    main()
+
--- uci.py
+++ uci.py
@@ -53,9 +53,13 @@ def get_data(df_X, df_y, train_edge_prob, train_y_prob, seed=0, normalize=True):
         min_max_scaler = preprocessing.MinMaxScaler()
         x_scaled = min_max_scaler.fit_transform(x)
         df_X = pd.DataFrame(x_scaled)
+    
+    print("Creating edges...")
     edge_start, edge_end = create_edge(df_X)
     edge_index = torch.tensor([edge_start, edge_end], dtype=int)
+    print("Creating edge attributes...")
     edge_attr = torch.tensor(create_edge_attr(df_X), dtype=torch.float)
+    print("Creating nodes...")
     node_init = create_node(df_X) 
     x = torch.tensor(node_init, dtype=torch.float)
     y = torch.tensor(df_y, dtype=torch.float)
@@ -86,4 +90,52 @@ def get_data(df_X, df_y, train_edge_prob, train_y_prob, seed=0, normalize=True):
     
     return data
 
+def get_data_separate(df_X, df_y, train_edge_prob, train_y_prob, seed=0, normalize=True):
+    if len(df_y.shape)==1:
+        df_y = df_y.to_numpy()
+    elif len(df_y.shape)==2:
+        df_y = df_y[0].to_numpy()
+
+    if normalize:
+        x = df_X.values
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        df_X = pd.DataFrame(x_scaled)
+    
+    print("Creating edges...")
+    edge_start, edge_end = create_edge(df_X)
+    edge_index = torch.tensor([edge_start, edge_end], dtype=int)
+    print("Creating edge attributes...")
+    edge_attr = torch.tensor(create_edge_attr(df_X), dtype=torch.float)
+    print("Creating nodes...")
+    node_init = create_node(df_X) 
+    x = torch.tensor(node_init, dtype=torch.float)
+    y = torch.tensor(df_y, dtype=torch.float)
+    
+    #set seed to fix known/unknwon edges
+    torch.manual_seed(seed)
+
+    #keep train_edge_prob of all edges
+    train_edge_mask = get_known_mask(train_edge_prob, int(edge_attr.shape[0]/2))
+    double_train_edge_mask = torch.cat((train_edge_mask, train_edge_mask), dim=0)
+
+    #mask edges based on the generated train_edge_mask
+    #train_edge_index is known, test_edge_index in unknwon, i.e. missing
+    train_edge_index, train_edge_attr = mask_edge(edge_index, edge_attr,
+                                                double_train_edge_mask, True)
+    test_edge_index, test_edge_attr = mask_edge(edge_index, edge_attr,
+                                                ~double_train_edge_mask, True)
+
+    #mask the y-values during training, i.e. how we split the training and test sets
+    #train_y_mask = get_known_mask(train_y_prob, y.shape[0])
+    #test_y_mask = ~train_y_mask
+
+    data = Data(x=x, y=y, edge_index=edge_index, edge_attr=edge_attr,
+            #train_y_mask=train_y_mask, test_y_mask=test_y_mask,
+            train_edge_mask = train_edge_mask,
+            train_edge_index=train_edge_index,train_edge_attr=train_edge_attr,
+            test_edge_index=test_edge_index,test_edge_attr=test_edge_attr,
+            df_X=df_X,df_y=df_y)
+    
+    return data
 
