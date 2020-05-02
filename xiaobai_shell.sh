532473a1c5229f93179b0810b864b6d24af59588
Xiaobai Ma <xiaobaima@DN2lk5dk9.stanford.edu>
2020-05-02 16:41:55 -0700

    test baseline lv0

--- xiaobai_shell.sh
+++ xiaobai_shell.sh
@@ -13,7 +13,12 @@
 # 	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.9 --train_y 0.9 --save_model --seed $seed --comment v2miss0.9
 # done
 
+# for seed in 0 1 2 3 4
+# do
+# 	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.3 --train_y 0.3 --save_model --seed $seed --comment v2miss0.3
+# done
+
 for seed in 0 1 2 3 4
 do
-	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.3 --train_y 0.3 --save_model --seed $seed --comment v2miss0.3
+	python baseline_uci_mdi_all.py --level 0 --seed $seed --comment v2lv0
 done
