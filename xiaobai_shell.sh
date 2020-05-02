89a311723b585417df7a0bb0fbad585132b52b7c
Xiaobai Ma <xiaobaima@DN2lk5dk9.stanford.edu>
2020-05-02 15:40:18 -0700

    miss 0.3

--- xiaobai_shell.sh
+++ xiaobai_shell.sh
@@ -8,7 +8,12 @@
 # 	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.5 --train_y 0.5 --save_model --seed $seed --comment v2miss0.5
 # done
 
+# for seed in 0 1 2 3 4
+# do
+# 	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.9 --train_y 0.9 --save_model --seed $seed --comment v2miss0.9
+# done
+
 for seed in 0 1 2 3 4
 do
-	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.9 --train_y 0.9 --save_model --seed $seed --comment v2miss0.9
+	python train_uci_mdi_all.py --post_hiddens 0 --train_edge 0.3 --train_y 0.3 --save_model --seed $seed --comment v2miss0.3
 done
