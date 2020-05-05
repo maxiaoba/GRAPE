1dbb834b78d0cee9d1f25242a28e15042fd6fc8d
Xiaobai Ma <xiaobaima@DNab4216ca.stanford.edu>
2020-05-05 01:54:56 -0700

    linear regression v2 test

--- xiaobai_shell.sh
+++ xiaobai_shell.sh
@@ -22,13 +22,21 @@
 # do
 # 	for seed in 0 1 2 3 4
 # 	do
-# 		python baseline_uci_mdi_all.py --best_level --train_edge $train --seed $seed --comment v2_best_train$train
+# 		python baseline_uci_mdi_all.py --best_level --train_edge $train --seed $seed --comment v2train$train
 # 	done
 # done
 
-for seed in 0 1 2 3 4
+# for seed in 0 1 2 3 4
+# do
+# 	python train_uci_y_all.py --post_hiddens 0 --train_edge 0.9 --seed $seed --comment v2train0.9
+# done
+
+for train in 0.3 0.5 0.7 0.9
 do
-	python train_uci_y_all.py --post_hiddens 0 --train_edge 0.9 --seed $seed --comment v2train0.9
+	for seed in 0 1 2 3 4
+	do
+		python linear_regression_uci_y_all.py --best_level --train_edge $train --seed $seed --comment v2train$train
+	done
 done
 
 
