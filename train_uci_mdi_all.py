f5fedc96b71413d558a4af84e0a2246f021d9902
Xiaobai Ma <xiaobaima@DN2lk5ee8.stanford.edu>
2020-05-12 23:40:46 -0700

    rerun known1.0 test

--- train_uci_mdi_all.py
+++ train_uci_mdi_all.py
@@ -76,8 +76,8 @@ def main():
     ## new
     # for args.data in ['concrete', 'energy', 'housing', 'kin8nm',
     #                 'naval', 'power', 'protein', 'wine', 'yacht']:
-    # for args.data in ['concrete', 'energy', 'housing', 'wine', 'yacht']:
-    for args.data in [ 'kin8nm', 'naval', 'power', 'protein']:
+    for args.data in ['concrete', 'energy', 'housing', 'wine', 'yacht']:
+    # for args.data in [ 'kin8nm', 'naval', 'power', 'protein']:
         data = load_data(args)
 
         log_path = './uci/mdi_results/results/gnn_mdi_{}/{}/{}/'.format(args.comment, args.data, args.seed)
