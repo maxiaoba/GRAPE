b189592edf55a7a5a8188affdcc694a779ff3bed
Xiaobai Ma <xiaobaima@Xiaobais-MacBook-Pro.local>
2020-05-29 00:50:36 -0700

    update plots and summary

--- load_uci_tables.py
+++ load_uci_tables.py
@@ -2,30 +2,40 @@ import os.path as osp
 import numpy as np
 import joblib
 
-pre_path = "./uci/mdi_results"
-# pre_path = "./uci/y_results"
-datasets = ["concrete","energy","housing","kin8nm","naval","power",
-			"protein","wine","yacht"]
+# pre_path = "./uci/mdi_results"
+pre_path = "./uci/y_results"
+
 # methods = ["knn","mean","mice","svd","spectral","gain","gnn_mdi"]
 # method_names = ["knn","mean","mice","svd","spectral","gain","gnn"]
+
 # methods = ["knn","mean","mice","svd","spectral","gain","gnn_mdi","gnn"]
 # method_names = ["knn","mean","mice","svd","spectral","gain","gnn","gnn n2n"]
+
+methods = ["gnn_v2train0.7","gnn_mdi_v2train0.7"]
+method_names = ["MD-GNN End-to-End","MD-GNN with Linear Regression"]
+table_name = 'n2n'
+
 # methods = ["gnn_mdi_v2train0.7","gnn_mdi_v2train0.7known1.0"]
-# method_names = ["with edge dropout","without edge dropout"]
-methods = ["knn_v2train0.7splitrandom0.7","knn_v2train0.7splitrandom0.7test",
-			"mean_v2train0.7splitrandom0.7","mean_v2train0.7splitrandom0.7test",
-			"mice_v2train0.7splitrandom0.7","mice_v2train0.7splitrandom0.7test",
-			"svd_v2train0.7splitrandom0.7","svd_v2train0.7splitrandom0.7test",
-			"spectral_v2train0.7splitrandom0.7","spectral_v2train0.7splitrandom0.7test",
-			"gain_v2train0.7splitrandom0.7","gain_v2train0.7splitrandom0.7train",
-			"gnn_mdi_v2train0.7splitrandom0.7train","gnn_mdi_v2train0.7splitrandom0.7test",
-			"gnn_mdi_v2train0.7splitrandom0.7traintest"
-			]
-method_names = ["knn","knn test","mean","mean test",
-				"mice","mice test","svd", "svd test",
-				"spectral","spectral test","gain","gain train",
-				"gnn train","gnn test","gnn train test",]
-table_name = 'train0.7splitrandom0.7'
+# method_names = ["MD-GNN with edge dropout","MD-GNN without edge dropout"]
+# table_name = 'edgedropout'
+
+# methods = ["knn_v2train0.7splitrandom0.7","knn_v2train0.7splitrandom0.7test",
+# 			"mean_v2train0.7splitrandom0.7","mean_v2train0.7splitrandom0.7test",
+# 			"mice_v2train0.7splitrandom0.7","mice_v2train0.7splitrandom0.7test",
+# 			"svd_v2train0.7splitrandom0.7","svd_v2train0.7splitrandom0.7test",
+# 			"spectral_v2train0.7splitrandom0.7","spectral_v2train0.7splitrandom0.7test",
+# 			"gain_v2train0.7splitrandom0.7","gain_v2train0.7splitrandom0.7train",
+# 			"gnn_mdi_v2train0.7splitrandom0.7train","gnn_mdi_v2train0.7splitrandom0.7test",
+# 			"gnn_mdi_v2train0.7splitrandom0.7traintest"
+# 			]
+# method_names = ["knn","knn test","mean","mean test",
+# 				"mice","mice test","svd", "svd test",
+# 				"spectral","spectral test","gain","gain train",
+# 				"gnn train","gnn test","gnn train test",]
+# table_name = 'train0.7splitrandom0.7'
+
+datasets = ["concrete","energy","housing","kin8nm","naval","power",
+			"protein","wine","yacht"]
 seeds = [0,1,2,3,4]
 
 with open("{}/tables/mae_{}.txt".format(pre_path,table_name), "w") as text_file:
@@ -42,12 +52,12 @@ with open("{}/tables/mae_{}.txt".format(pre_path,table_name), "w") as text_file:
 			for seed in seeds:
 				load_path = '{}/results/{}/{}/{}/'.format(pre_path,method, dataset, seed)
 				obj = joblib.load(load_path+'result.pkl')
-				if method.startswith('gnn_mdi'):
-				# if method == 'gnn':
+				# if method.startswith('gnn_mdi'):
+				if method.startswith('gnn') and not method.startswith('gnn_mdi'):
 					result.append(obj['curves']['test_l1'][-1])
 				elif method.startswith('gain'):
-					result.append(obj['mdi_mae'])
-					# result.append(obj['reg_mae'])
+					# result.append(obj['mdi_mae'])
+					result.append(obj['reg_mae'])
 				else:
 					result.append(obj['mae'])
 			mean = np.mean(result)
--- plot_uci_results.py
+++ plot_uci_results.py
@@ -2,38 +2,40 @@ import os.path as osp
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
+import tikzplotlib
 from matplotlib.ticker import FormatStrFormatter
 import seaborn as sns
 sns.set_context("poster")
 sns.set_style("ticks")
 
+pre_path = "./uci/mdi_results"
+methods = ["knn","mean","mice","svd","spectral","gain","gnn_mdi"]
+method_names = ["KNN","Mean","MICE","SVD","Spectral","GAIN","MD-GNN"]
+colors = [(0., 0., 0.5), 'g', 'c', 'm', 'y', 'pink', 'blue']
+ylabel = 'MDI Test MAE'
+plot_name = 'mdi_known_ratio'
 
-
-# pre_path = "./uci/mdi_results"
-# methods = ["knn","mean","mice","svd","spectral","gain","gnn_mdi"]
-# method_names = ["KNN","Mean","MICE","SVD","Spectral","GAIN","GNN"]
+# pre_path = "./uci/y_results"
+# methods = ["knn","mean","mice","svd","spectral","gain","gnn"]
+# method_names = ["KNN","Mean","MICE","SVD","Spectral","GAIN","MD-GNN"]
 # colors = [(0., 0., 0.5), 'g', 'c', 'm', 'y', 'pink', 'blue']
-# plot_name = 'mdi_known_ratio'
+# ylabel = "Downstream Regression Test MAE"
+# plot_name = 'reg_known_ratio'
 
-pre_path = "./uci/y_results"
-methods = ["knn","mean","mice","svd","spectral","gain","gnn_mdi","gnn"]
-method_names = ["knn","mean","mice","svd","spectral","gain","gnn","gnn n2n"]
-colors = [(0., 0., 0.5), 'g', 'c', 'm', 'y', 'pink', 'blue', 'red']
-plot_name = 'reg_known_ratio'
-
-# comments = ['v2train0.9','v2train0.7','v2train0.5','v2train0.3']
-comments = ['v2train0.3','v2train0.5','v2train0.7','v2train0.9']
-# xs = [0.1,0.3,0.5,0.7]
-xs = [0.7,0.5,0.3,0.1]
-datasets = ["concrete","energy","housing","kin8nm","naval","power",
-			"protein","wine","yacht"]
+comments = ['v2train0.9','v2train0.7','v2train0.5','v2train0.3']
+xs = [0.1,0.3,0.5,0.7]
+# datasets = ["concrete","energy","housing","kin8nm","naval","power",
+# 			"protein","wine","yacht"]
+# datasets = ["concrete"]
+datasets = ["protein"]
 seeds = [0,1,2,3,4]
 xlabel = 'Missing data ratio'
-ylabel = 'Test MAE'
+use_ylabel = False
+use_legend = True
 
 for dataset in datasets:
 	print(dataset)
-	plt.figure(figsize=(10,14))
+	plt.figure(figsize=(10,8))
 	for method,method_name, color in zip(methods,method_names,colors):
 		ys = []
 		for comment in comments:
@@ -41,26 +43,34 @@ for dataset in datasets:
 			for seed in seeds:
 				load_path = '{}/results/{}_{}/{}/{}/'.format(pre_path, method, comment, dataset, seed)
 				obj = joblib.load(load_path+'result.pkl')
-				# if method == 'gnn_mdi':
-				if method == 'gnn':
+				if method == 'gnn_mdi':
+				# if method == 'gnn':
 					result.append(obj['curves']['test_l1'][-1])
 				elif method == 'gain':
-					# result.append(obj['mdi_mae'])
-					result.append(obj['reg_mae'])
+					result.append(obj['mdi_mae'])
+					# result.append(obj['reg_mae'])
 				else:
 					result.append(obj['mae'])
 			mean = np.mean(result)
 			std = np.std(result)
 			ys.append(mean)
 		plt.errorbar(xs, ys, std, label=method_name, color=color)
-	plt.legend(prop={'size': 28})
+	if use_legend:
+		plt.legend(loc='right', bbox_to_anchor=(1.4, 0.5), ncol=1,prop={'size': 28})
 	plt.xlabel(xlabel, fontsize=36)
-	plt.ylabel(ylabel, fontsize=36)
-	plt.xticks([0.1, 0.3, 0.5, 0.7], fontsize=30)
+	if use_ylabel:
+		plt.ylabel(ylabel, fontsize=36)
+	plt.xticks(xs, fontsize=30)
 	plt.yticks(fontsize=30)
 	ax = plt.gca()
 	ax.xaxis.set_major_formatter(FormatStrFormatter('%0.2f'))
 	ax.yaxis.set_major_formatter(FormatStrFormatter('%0.2f'))
 	plt.title(dataset, fontsize=36)
-	plt.savefig("{}/plots/{}_{}.png".format(pre_path,plot_name,dataset), dpi=150, bbox_inches='tight')
+	plot_file = "{}/plots/{}_{}".format(pre_path,plot_name,dataset)
+	if use_ylabel:
+		plot_file = plot_file + "_label"
+	if use_legend:
+		plot_file = plot_file + '_legend'
+	plt.savefig("{}.png".format(plot_file), dpi=150, bbox_inches='tight')
+	tikzplotlib.save("{}.tex".format(plot_file))
 
